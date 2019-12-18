a33f89c762f15bc5124c1592ad7f338cb8e1c48a
Govind Mulgir <govind.mulgir@rajasoftwarelabs.com>
2019-12-18 14:18:27 +0530

    Initial Commit

---
+++ BitriseDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BitriseDemo
+//
+//  Created by Admin on 18/12/19.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
