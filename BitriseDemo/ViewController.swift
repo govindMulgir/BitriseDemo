b652cfaf4fd0bcd40cd861574c4ae31f8975acc9
Govind Mulgir <govind.mulgir@rajasoftwarelabs.com>
2019-12-18 17:11:42 +0530

    Test commit

--- BitriseDemo/ViewController.swift
+++ BitriseDemo/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .gray
         // Do any additional setup after loading the view.
+        
+        print("Hello")
     }
 
 
